08dd5cfaf3ea5eb63e0d920195f89fe16e8fddc3
corevirus-ops <corevirus1738@gmail.com>
2026-09-19 14:31:58 -0700

    changed reference to django base user, applied redirect to login when user is not authenticated

--- todos/models.py
+++ todos/models.py
@@ -1,17 +1,11 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 
-class User(models.Model):
-    username = models.CharField(max_length=100)
-    email = models.EmailField()
-    password = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.username
 
 class Todo(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='todos')
     title = models.CharField(max_length=100)
     description = models.TextField()
     completed = models.BooleanField(default=False)
--- todos/views.py
+++ todos/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    if request.user.is_authenticated:
+        return render(request, 'todos/login.html')
     return render(request, 'todos/index.html')
 
 
