1c6ba768cfe5908bbe10bd4dabdcdec244613d58
corevirus-ops <corevirus1738@gmail.com>
2026-09-19 14:20:52 -0700

    created user db and todos db items, created setup to render html page

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=100)
+    email = models.EmailField()
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.username
+
+class Todo(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
